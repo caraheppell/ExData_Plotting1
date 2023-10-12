4b247b1e39b3815d5b112a3e883db29cc9a3fbfa
caraheppell <caraheppell@gmail.com>
2023-10-12 20:03:23 +0100

    plots

---
+++ plot2.R
@@ -0,0 +1,5 @@
+Sys.setlocale("LC_TIME", "en_US")
+date_converted <- as.Date(data[, 1], format = "%d/%m/%Y")
+time_converted <- strptime(data[, 2], format = "%H:%M:%S")
+time_axis <- as.POSIXct(paste(date_converted, data[, 2]))
+plot(time_axis, data[, 3], xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
